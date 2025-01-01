c700dc8255e0dc0c53446a7a63ab8606b40553b1
UltimadeiGit <sam.shakeshaft@outlook.com>
2025-01-01 23:54:01 +0000

    Pages attempt 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    trailingSlash: true, // Ensures paths like `/about/` instead of `/about`
+     basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
 }
 
 module.exports = nextConfig;

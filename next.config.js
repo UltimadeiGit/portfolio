086147840553cdf1a9992aed69b7ecc1ee79a3ff
UltimadeiGit <sam.shakeshaft@outlook.com>
2025-01-02 02:53:02 +0000

    Updated site to first acceptable version (with fixed gh-pages css)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    //basePath: '/portfolio', // Replace 'portfolio' with your repo name
-    //assetPrefix: '/portfolio', // Prefix for static assets
+    basePath: '/portfolio', // Where 'portfolio' is the repo name
+    assetPrefix: '/portfolio', // Prefix for static assets
     trailingSlash: true, // Ensures directories end with '/'
     images: {
         unoptimized: true, // Disable image optimization for static exports
